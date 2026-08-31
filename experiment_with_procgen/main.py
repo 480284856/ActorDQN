9488b81ab7d7c875c592cea79373fbb9031f780e
Jay <480284856@qq.com>
2026-08-31 14:20:48 +0200

    vanilla dqn-first exp with maze

--- experiment_with_procgen/main.py
+++ experiment_with_procgen/main.py
@@ -5,20 +5,20 @@ from stable_baselines3.common.callbacks import EvalCallback
 
 def main():
     train_env = SubprocVecEnv(
-        [env_factory(rank, 'coinrun', 200) for rank in range(N_ENVS)],
+        [env_factory(rank, 'maze', 200) for rank in range(N_ENVS)],
         start_method="spawn",
     )
     eval_env = SubprocVecEnv(
-            [env_factory(rank, 'coinrun', 0) for rank in range(N_ENVS)],
+            [env_factory(rank, 'maze', 0) for rank in range(N_ENVS)],
             start_method="spawn",
         )
     train_env = VecMonitor(train_env, info_keywords=("is_success",))
     eval_env = VecMonitor(eval_env, info_keywords=("is_success",))
 
     train_freq = 4
-    eval_callback = EvalCallback(eval_env, best_model_save_path="./logs/",
-                             log_path="./logs/", eval_freq=1000*train_freq+1,
-                             n_eval_episodes=1000,
+    eval_callback = EvalCallback(eval_env, best_model_save_path="./logs/maze",
+                             log_path="./logs/maze", eval_freq=10000*train_freq+1,
+                             n_eval_episodes=100,
                              deterministic=True, render=False)
     
     model = make_agent(train_env, seed=42)
